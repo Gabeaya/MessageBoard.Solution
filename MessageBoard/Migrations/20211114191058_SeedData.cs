8e32601d724e379c006ec6be0c3f09400f395cc7
Gabriel Ayala <gabeayala100@gmail.com>
2021-11-14 16:47:11 -0800

    Add it.

---
+++ MessageBoard/Migrations/20211114191058_SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MessageBoard.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "MessageId", "Age", "Date", "MessageDescription", "UserName" },
+                values: new object[,]
+                {
+                    { 1, 17, "11-14-2021", "hey pablo ", "Matilda" },
+                    { 2, 21, "06-31-1908", "oooooh you nasty", "Rexie" },
+                    { 3, 35, "09-26-1998", "who are you sisi", "Matilda" },
+                    { 4, 15, "02-10-1996", "My butt itches", "Pip" },
+                    { 5, 22, "11-15-1999", "ya that sisi is one looker", "Bartholomew" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 5);
+        }
+    }
+}
